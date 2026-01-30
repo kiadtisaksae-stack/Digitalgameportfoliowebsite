99e7658453fd1bd4e0c3a4cb449250550f523a75
kiadtisaksae-stack <kiadtisak.sae@spumail.net>
2026-01-30 23:02:36 +0700

    Reorganize sections in App component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Hero } from './components/Hero';
-import { ModelsSection } from './components/ModelsSection';
 import { GamesSection } from './components/GamesSection';
-import { GameDesignSection } from './components/GameDesignSection'; // ไฟล์ใหม่
-import { AnimationSection } from './components/AnimationSection'; // ไฟล์ใหม่
+import { GameDesignSection } from './components/GameDesignSection';
+import { AnimationSection } from './components/AnimationSection';
+import { ModelsSection } from './components/ModelsSection';
 import { SkillsSection } from './components/SkillsSection';
 import { Footer } from './components/Footer';
 import { MiniGame } from './components/MiniGame';
@@ -11,11 +11,13 @@ export default function App() {
   return (
     <div className="min-h-screen bg-black text-white">
       <Hero />
-      <GamesSection />
-      <GameDesignSection />
-      <AnimationSection />
-      <ModelsSection />
-      <SkillsSection />
+      <div className="space-y-0">
+        <GamesSection />
+        <GameDesignSection />
+        <AnimationSection />
+        <ModelsSection />
+        <SkillsSection />
+      </div>
       <Footer />
       <MiniGame />
     </div>
