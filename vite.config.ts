404ec8ddf9a2c782f4a9752db5aa2ad0a5ad18a1
kiadtisaksae-stack <kiadtisak.sae@spumail.net>
2026-01-30 21:09:18 +0700

    vite
    f

--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,20 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/Digitalgameportfoliowebsite/', // ต้องตรงกับชื่อ Repository ของคุณ
+  base: '/Digitalgameportfoliowebsite/', 
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
-      'vaul@1.1.2': 'vaul',
-      'sonner@2.0.3': 'sonner',
-      'recharts@2.15.2': 'recharts',
-      'react-resizable-panels@2.1.7': 'react-resizable-panels',
-      'react-hook-form@7.55.0': 'react-hook-form',
-      'react-day-picker@8.10.1': 'react-day-picker',
-      'next-themes@0.4.6': 'next-themes',
-      'lucide-react@0.487.0': 'lucide-react',
-      'input-otp@1.4.2': 'input-otp',
-      'embla-carousel-react@8.6.0': 'embla-carousel-react',
-      'cmdk@1.1.1': 'cmdk',
-      'class-variance-authority@0.7.1': 'class-variance-authority',
       '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
     target: 'esnext',
-    outDir: 'dist', // กำหนดให้สร้างโฟลเดอร์ dist
+    outDir: 'dist', 
+  },
+  server: {
+    port: 3000,
+    open: true,
   },
 });
